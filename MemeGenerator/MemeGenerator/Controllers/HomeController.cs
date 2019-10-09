b69bdc26490971b5ed45eb16c8a26fd3193957ae
Jason Lu <jasonluX13@gmail.com>
2019-10-09 10:27:23 -0400

    Initial homepage and templates with list of images

---
+++ MemeGenerator/MemeGenerator/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MemeGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        // List of completed memes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Templates()
+        {
+            return View();
+        }
+    }
+}
