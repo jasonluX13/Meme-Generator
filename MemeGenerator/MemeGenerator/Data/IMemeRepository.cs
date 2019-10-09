be9262e90cbb9c2e07c960b4a7c24d39b1e565d1
Adam Rzonca <a.rzonca@yahoo.com>
2019-10-09 10:50:15 -0400

    Added files for context, meme repository, template model and user model

---
+++ MemeGenerator/MemeGenerator/Data/IMemeRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MemeGenerator.Data
+{
+    public interface IMemeRepository
+    {
+    }
+}
---
+++ MemeGenerator/MemeGenerator/Data/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MemeGenerator.Data
+{
+    public class User
+    {
+
+        int Id { get; set; }
+        [Required]
+        string Username { get; set; }
+        [Required]
+        string HashedPassword { get; set; }
+
+    }
+}
