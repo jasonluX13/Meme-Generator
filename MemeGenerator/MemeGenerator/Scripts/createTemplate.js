00f8cba356a18fa8050ec1244266257dc8ada7ad
Jason Lu <jasonluX13@gmail.com>
2019-10-10 15:15:59 -0400

    Added ability to clear text while creating a template

--- MemeGenerator/MemeGenerator/Scripts/createTemplate.js
+++ MemeGenerator/MemeGenerator/Scripts/createTemplate.js
@@ -4,7 +4,8 @@ const ctx = canvas.getContext('2d');
 const width = 400;
 canvas.crossOrigin = "Anonymous";
 
-document.getElementById('addTextboxes').addEventListener('click', function () {
+ctx.fillStyle = "black";
+function addText() {
     let src = document.getElementById('url').value;
     document.getElementById('image').innerHTML = '<img id="img" style="display: none" class="meme-img" src="' + src + '" />';
     let img = document.getElementById('img');
@@ -12,8 +13,10 @@ document.getElementById('addTextboxes').addEventListener('click', function () {
     canvas.width = width;
     canvas.height = width * img.height / img.width;
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    document.getElementById('addTextboxes').style.display = 'none';
+    document.getElementById('clear').style.display = '';
+}
 
-});
 
 function getCursorPosition(canvas, event) {
     const rect = canvas.getBoundingClientRect()
@@ -21,11 +24,21 @@ function getCursorPosition(canvas, event) {
 
     const x = event.clientX - rect.left
     const y = event.clientY - rect.top
+    ctx.font = "20pt Verdana";
+    ctx.fillText("text", x, y);
     textboxlist.innerHTML += '<input type="text" name="coords" value="'+x+','+y +'" />'
     console.log("x: " + x + " y: " + y)
     document.getElementById('submit').disabled = false;
+   
 }
 
+function clear() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    document.getElementById('textboxes').innerHTML = '';
+}
 canvas.addEventListener('mousedown', function(e) {
     getCursorPosition(canvas, e)
 })
+document.getElementById('addTextboxes').addEventListener('click', addText);
+document.getElementById('clear').addEventListener('click', clear);
