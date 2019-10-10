bc39208b8c3ff68f57b6836068b68ea070b1f419
Adam Rzonca <a.rzonca@yahoo.com>
2019-10-10 09:53:15 -0400

    Implemented search for the index page

--- MemeGenerator/MemeGenerator/Scripts/index.js
+++ MemeGenerator/MemeGenerator/Scripts/index.js
@@ -7,11 +7,39 @@
     let data = await response.json();
     let memeList = data.data.memes;
     console.log(memeList);
-
+    let memeListfull = memeList;
     let list = document.getElementById('list');
+    
 
-    let imgList = memeList.map(meme => '<li><img class="meme-img" src="' + meme.url + '"/></li>');
-    let innerHtml = imgList.join('');
-    console.log(innerHtml);
-    list.innerHTML = innerHtml;
+    async function displayMemes(){
+        let imgList = memeList.map(meme => '<li><img class="meme-img" src="' + meme.url + '"/></li>');
+        let innerHtml = imgList.join('');
+        console.log(innerHtml);
+        list.innerHTML = innerHtml;
+    }
+
+    async function filterMemes() {
+        let searchCriteria = document.getElementById("filter").value;
+        let result = [];
+        for(let i=0;i<memeListfull.length;i++){
+            
+            if(memeListfull[i].name.includes(searchCriteria)) result.push(memeListfull[i]);
+        }
+        memeList = result;
+        list.innerHTML = "";
+        displayMemes(); 
+    }
+
+    async function clearFilter(){
+        memeList = memeListfull;
+        list.innerHTML="";
+        displayMemes();
+    }
+
+    displayMemes();
+    document.getElementById("Search").addEventListener("click", filterMemes);
+    document.getElementById("Clear").addEventListener("click", clearFilter);
+
+
+
 })();
