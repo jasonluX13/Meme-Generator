89ef353a796736e87d85fabe1ea5801929d4382c
Jason Lu <jasonluX13@gmail.com>
2019-10-14 10:25:03 -0400

    Text wrap on details page

--- MemeGenerator/MemeGenerator/Scripts/details.js
+++ MemeGenerator/MemeGenerator/Scripts/details.js
@@ -10,6 +10,26 @@
     ctx.fillStyle = "black";
     ctx.font = "20pt Verdana";
 
+    function wrapText(context, text, x, y, maxWidth, lineHeight) {
+        var words = text.split(' ');
+        var line = '';
+
+        for (var n = 0; n < words.length; n++) {
+            var testLine = line + words[n] + ' ';
+            var metrics = context.measureText(testLine);
+            var testWidth = metrics.width;
+            if (testWidth > maxWidth && n > 0) {
+                context.fillText(line, x, y);
+                line = words[n] + ' ';
+                y += lineHeight;
+            }
+            else {
+                line = testLine;
+            }
+        }
+        context.fillText(line, x, y);
+    }
+
     let url = "http://localhost:53520/api/memes/meme/" + img.getAttribute('data-id');
     console.log(url);
 
@@ -22,6 +42,7 @@
         let x = textboxes[i].X;
         let y = textboxes[i].Y;
         let text = textboxes[i].Text;
-        ctx.fillText(text,x,y);
+        //ctx.fillText(text,x,y);
+        wrapText(ctx, text, x, y, 100, 20);
     }
 })();
