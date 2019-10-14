d704827da58a4e57f1575359efc79500b0a4f8a3
Adam Rzonca <a.rzonca@yahoo.com>
2019-10-14 10:16:01 -0400

    Added comment functionality

--- MemeGenerator/MemeGenerator/Controllers/MemeController.cs
+++ MemeGenerator/MemeGenerator/Controllers/MemeController.cs
@@ -12,9 +12,11 @@ namespace MemeGenerator.Controllers
     public class MemeController : Controller
     {
         private IMemeRepository _memeRepo;
-        public MemeController(IMemeRepository memeRepo)
+        private IUserRepository _userRepo;
+        public MemeController(IMemeRepository memeRepo, IUserRepository userRepo)
         {
             _memeRepo = memeRepo;
+            _userRepo = userRepo;
         }
 
         // GET: Meme
@@ -32,6 +34,35 @@ namespace MemeGenerator.Controllers
             }
             MemeResponse meme  = await _memeRepo.GetMemeAsync((int)id);
             return View(meme);
+        }
+
+        public ActionResult Comment(int id)
+        {
+            Comment comment = new Comment();
+            return View("Comment", comment);
+        }
+
+        [ValidateAntiForgeryToken, HttpPost]
+        async public Task<ActionResult> Comment(int id, [Bind(Include = "Id, Text, Rating, CreatorId")] Comment comment)
+        {
+            if (string.IsNullOrWhiteSpace(comment.Text))
+            {
+                ModelState.AddModelError("", "Text can't be empty");
+            }
+            if (ModelState.IsValid)
+            {
+                User creator = _userRepo.GetByUsername(User.Identity.Name);
+
+                Comment newComment = new Comment()
+                {
+                    CreatorId = creator.Id,
+                    MemeId = id,
+                    Text = comment.Text,
+                };
+                await _memeRepo.AddCommentAsync(newComment);
+                return RedirectToAction("Details", new { id = id });
+            }
+            return View("Comment");
         }
 
     }
